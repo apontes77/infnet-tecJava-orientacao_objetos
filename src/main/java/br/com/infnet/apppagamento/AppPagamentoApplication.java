f20dd669116dbdbe16190fc73d7b1db97ba8a93f
Alexandre <alexandre@pop-os.localdomain>
2022-08-03 19:06:22 -0300

    primeiro commit do projeto

---
+++ src/main/java/br/com/infnet/apppagamento/AppPagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.infnet.apppagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPagamentoApplication.class, args);
+	}
+
+}
